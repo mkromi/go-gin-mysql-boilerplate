1ecd1fa8cba3c2850c50ad70249d3ab701b4767a
Mohiuddin Khan <mohiuddinkhanromi@gmail.com>
2021-01-21 19:10:10 +0600

    Changes to readme

--- Middlewares/Auth.go
+++ Middlewares/Auth.go
@@ -13,8 +13,10 @@ func IsClientAuthenticated(c *gin.Context) {
 	user, password, hasAuth := c.Request.BasicAuth()
 	if hasAuth {
 		var clientUser Schema.OAuthClient
-		if err := Config.DB.Raw("SELECT * FROM o_auth_clients " +
-			"WHERE name = ? and secret = ?", user, password).Scan(&clientUser).Error; err != nil {
+		if err := Config.DB.
+			Raw("select * from o_auth_clients where name = ? and secret = ?", user, password).
+			Scan(&clientUser).
+			Error; err != nil {
 			Services.Unauthorized(c, "Authentication Failed!", nil)
 			return
 		}
@@ -29,11 +31,18 @@ func IsUserAuthenticated(c *gin.Context) {
 		Services.Unauthorized(c, "Authentication Failed!", nil)
 		return
 	}
-	req := c.Request.Header.Get("Authorization")
 
+	req := c.Request.Header.Get("Authorization")
 	var access Schema.OAuthAccessToken
-	if err := Config.DB.Raw("SELECT * FROM o_auth_access_tokens " +
-		"WHERE access_token = ? and expired_at > ? and revoked = ?", strings.Split(req, " ")[1], time.Now().Format("2006-01-02 15:04:05"), false).Scan(&access).Error; err != nil {
+	if err := Config.DB.
+		Raw(
+			"select * from o_auth_access_tokens where access_token = ? and expired_at > ? and revoked = ?",
+			strings.Split(req, " ")[1],
+			time.Now().Format("2006-01-02 15:04:05"),
+			false,
+		).
+		Scan(&access).
+		Error; err != nil {
 		Services.Unauthorized(c, "Invalid Token!", nil)
 		return
 	}
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # go-gin-mysql-boilerplate
+> A starter project with Golang, Gin and MySQL
+
+Golang Gin boilerplate with MySQL resource. Authenticated with JWT bearer and basic authentication.
+
+### Boilerplate structure
+
+```
+.
+├── Config
+│   └── Database.go
+├── Controllers
+│   ├── Auth.go
+│   └── User.go
+├── Middlewares
+│   └── Auth.go
+├── Models
+│   ├── Auth.go
+│   ├── Schema
+│   │   ├── OAuthAccessToken.go
+│   │   ├── OAuthClient.go
+│   │   ├── OAuthRefreshToken.go
+│   │   └── User.go
+│   └── User.go
+├── Routes
+│   └── Routes.go
+├── Services
+│   ├── Auth.go
+│   └── Response.go
+├── Validations
+│   ├── Auth.go
+│   └── User.go
+├── go.mod
+├── go.sum
+├── LICENSE
+├── README.md
+└── main.go
+
+```
+
+## Installation
+
+```sh
+go run main.go
+```
+
+## Usage example
+
+`curl http://localhost:8080/users`
+
+### Dependencies:
+* [cors v1.3.1](github.com/gin-contrib/cors)
+* [gin v1.6.3](github.com/gin-gonic/gin)
+* [mysql v1.5.0](github.com/go-sql-driver/mysql)
+* [gorm v1.5.0](github.com/jinzhu/gorm)
+* [crypto v0.0.0-20191205180655-e7c4368fe9dd](golang.org/x/crypto)
+
+### **License**
+The **linux-live-wallpaper** is a open-source application licensed under the [MIT License](LICENSE).
